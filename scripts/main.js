27a6ab3237bdff6e5d0631bee30a72d32ea8cd78
edgar <edgar.ad.martinez@gmail.com>
2025-07-24 20:20:43 -0400

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+
+
+// for scroll behaviour
+const whatSection = document.getElementById("what");
+const whatBounds = whatSection.getBoundingClientRect()
+
+function scrollToWhat() {
+    console.log("Scrolling to", whatBounds.y)
+    scrollTo(0, whatBounds.y)
+}
+
+explainBtn.addEventListener("click", scrollToWhat)
+
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
+
+
+// for download buttons
+const downloadPage = "https://google.com"
+const downloadBtns = document.getElementsByClassName("downloadBtn");
+const explainBtn = document.getElementById("explain")
+
+function visitDownloadPage() {
+    window.open(downloadPage, '_blank').focus();
+}
+
+for (const btn of downloadBtns) {
+    btn.addEventListener("click", visitDownloadPage)
+}
+
