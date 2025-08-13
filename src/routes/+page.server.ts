aefeabbb4af8eb9c88eb593a7a69559a471bca4e
edgar <edgar.ad.martinez@gmail.com>
2025-08-13 19:50:03 -0400

    commenting out server code to see what happens

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,36 @@
 import type { Actions } from './$types';
 
-export const actions: Actions = {
-  signup: async ({ request }) => {
-    console.log('Form submitted');
-    return { success: true };
+export const actions = {
+    waitlist: addEmail
+} satisfies Actions
+
+async function addEmail({ request }: any) {
+  try {
+    console.log("Adding email")
+    const data = await request.formData();
+    const email = data.get("email")
+
+    console.log("Email: ", email)
+    
+    // Basic validation
+    if (!email || !email.includes('@') || email.length > 100) {
+      console.error("No email")
+      return {
+        status: "error",
+        message: "Email is too long, or is incorrectly formatted."
+      };
+    }
+    
+    return {
+      status: 'success',
+      message: 'Email validation passed!'
+    };
+    
+  } catch (err: any) {
+    console.error('Waitlist error:', err);
+    return {
+      status: "error",
+      message: "Something went wrong"
+    };
   }
-};
+}
